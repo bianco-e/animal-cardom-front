8bfe5d4abc21cd42fd53f479d2c7fbe526674db9
bianco-e <bianco.emiliozzi@gmail.com>
2022-07-28 12:58:27 +0200

    update user file name to auth

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react"
 import { useEffect } from "react"
 import { useHistory } from "react-router"
 import { AuthUser } from "../interfaces"
-import { loginUser } from "../redux/actions/user"
+import { loginUser } from "../redux/actions/auth"
 import { useAppDispatch } from "./redux-hooks"
 
 export default function useLogin() {
